96dd0decf2b71c420a02757f8b7ded8f80ef7a26
Raihan Putra I <raihanputrairawan@gmail.com>
2026-09-25 13:05:08 +0700

    chore: update footer link from deleted Motor Story to Philosophy & Ethos

--- Documents/KAMPUS/SAKALA_PROJECT/src/components/layout/Footer.tsx
+++ Documents/KAMPUS/SAKALA_PROJECT/src/components/layout/Footer.tsx
@@ -66,7 +66,7 @@ export default function Footer() {
               </li>
               <li>
                 <Link href="/about" className="hover:text-white transition-colors">
-                  The Motor Story Protocol
+                  Philosophy & Ethos
                 </Link>
               </li>
               <li>
