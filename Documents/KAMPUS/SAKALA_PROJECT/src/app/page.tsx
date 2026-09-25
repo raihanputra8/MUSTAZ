816c6477a5bd25b91692ab76f5b20f3e25b7071c
Raihan Putra I <raihanputrairawan@gmail.com>
2026-09-26 01:40:06 +0700

    fix(hero): restore original four pillars gateway hero banner (PillarsGatewaySection)

--- Documents/KAMPUS/SAKALA_PROJECT/src/app/page.tsx
+++ Documents/KAMPUS/SAKALA_PROJECT/src/app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '@/components/layout/Navbar';
+import PillarsGatewaySection from '@/components/home/PillarsGatewaySection';
 import HeroSection from '@/components/home/HeroSection';
 import GarageSection from '@/components/home/GarageSection';
 import SupplySection from '@/components/home/SupplySection';
@@ -21,6 +22,7 @@ export default async function HomePage() {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-1">
+        <PillarsGatewaySection />
         <HeroSection />
         <GarageSection bikes={bikes} />
         <SupplySection initialProducts={products} />
